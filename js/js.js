3b29468f2c7b963c478be1ced2ad9e6f9e019a0b
tomeknas <tomeknas@o2.pl>
2017-07-30 20:56:43 +0200

    Create addProduct function and local storage

--- js/js.js
+++ js/js.js
@@ -3,6 +3,47 @@
 var app = angular.module('app', ['angular-storage']);
 	app.controller('ctrl', ['$scope', 'store', function($scope, store){
 		
+    var inBasket=0;
+    var qa=0;
+    if (store.get('cart')) {
+          $scope.order = store.get('cart'); 
+          for (var i = 0; i < $scope.order.length; i++){
+        inBasket += $scope.order[i].qty*$scope.order[i].price;
+        qa += parseInt($scope.order[i].qty);
+
+          };
+        }else{
+          $scope.order=[];
+        }
+    $scope.totalPrice = inBasket;
+    $scope.totalQty = qa;
+
+
+
+    $scope.addProduct = function(product){
+          var wzor = /^[0-9]+$/;
+          if(product.name && wzor.test(product.qty) && wzor.test(product.price)){
+
+              $scope.order.push({
+              name:product.name,
+              qty:product.qty,
+              price:product.price 
+            });
+              $scope.summ = product.qty*product.price;
+              $scope.product = [];
+              $scope.totalPrice += $scope.summ;
+              $scope.totalQty += parseInt(product.qty);
+              store.set('cart', $scope.order);
+          }
+            else{
+              return false;
+            }
+   };
+
+    
+    $scope.clearBasket=function(){
+
+    }
 		
 
     
